1ca22648b9a0490e185a3721a8e1de8f38b8872e
jbui <jbui@class-docker>
2025-03-20 03:47:36 +0000

    update main.sh script

--- src/main.sh
+++ src/main.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Install dependencies from requirements.txt
-pip3 install -r requirements.txt
-
 # Change to appropriate directory 
 cd /workspace/simulateRecover/Simulate-Recover/src/
 
